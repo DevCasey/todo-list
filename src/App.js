4af0ad92cf33b8af1bfbeb3d06485eef8a56762c
Casey Calkins <c.angelcalkins@outlook.com>
2020-01-12 17:15:44 -0600

    Casey - Todo app finished

--- src/App.js
+++ src/App.js
@@ -9,23 +9,36 @@ class ToDo extends Component {
     }
   }
 
-  toggle = () => {
-    this.setState({isOn: !this.state.isOn})
-  }
+  // toggle = () => {
+  //   this.setState({isOn: !this.state.isOn})
+  // }
 
-  inputUpdate = (event) => {
+  inputUpdate = event => {
     this.setState({input: event.target.value})
   }
 
+  formSubmit = event => {
+    event.preventDefault()
+    console.log("THIS IS STATE", this.state.items)
+    this.setState({
+      items : [...this.state.items, this.state.input],
+      input: ''
+    })
+  }
+
   render () {
-    console.log('THIS IS STATE.', this.state.isOn)
     return (
       <div className="todo">
         <div className="main">
-          <form>
-              <input value={this.state.input} onChange={this.inputUpdate}></input>
-              <button onClick={this.toggle}>Submit</button>
+          <form onSubmit={this.formSubmit}>
+              <input placeholder="Enter Task" value={this.state.input} onChange={this.inputUpdate}></input>
+              <button>Submit</button>
           </form>
+            <ul>
+              {this.state.items.map((item, index) => {
+                return <li key={index}>{item}</li>
+              })}
+            </ul>
         </div>
       </div>
     )
